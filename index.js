74d130a3e0fe6c45b9f5aef85fdc0ea70ebe5541
Rohith Thirunagari <trohith.2006@gmail.com>
2024-07-03 11:30:47 +0530

    logger added

--- index.js
+++ index.js
@@ -73,6 +73,14 @@ app.use((req, res, next) => {
   res.status(404).send("404 - Not Found");
 });
 
+function logger(req, res, next){
+    console.log("ip:", req.ip);
+    console.log("method:", req.method);
+    console.log("hostname:", req.hostname);
+    console.log("date:", new Date());
+    next();
+}
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
